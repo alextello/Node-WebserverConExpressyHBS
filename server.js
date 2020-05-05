27d93e084907bc4c3b19eaa8aafafbdd69e63787
unknown <alextello1@hotmail.com>
2020-05-05 14:33:26 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+
+// Registrar Helpers
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+ 
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS Engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.get('/',  (req, res) => {
+    res.render('home', {nombre: 'Alex'});
+})
+
+app.get('/about',  (req, res) => {
+    res.render('about');
+})
+ 
+app.listen(port, () => {
+    console.log('Escuchando petiticiones en el puerto ' + port);
+});
